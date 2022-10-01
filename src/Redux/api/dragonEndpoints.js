d7a272f95e349da49b85bdb2f68d20fb08190c63
Andrii Porkhun <andrewporkhun8@gmail.com>
2022-10-01 23:28:41 +0300

    _redirect add

--- src/Redux/api/dragonEndpoints.js
+++ src/Redux/api/dragonEndpoints.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-// const reaqestedUserId = () => {
+// const requestedUserId = () => {
 //     let token = localStorage.getItem('token')
 //     let decoded = jwt_decode(token.token);
 
